7d7fe3b4f44f6eae87904bc304947f02534fd92b
qqatya <catherineburger@yandex.ru>
2023-07-06 00:44:07 +0300

    feat: добавить контроллер по рейтингам

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.RatingNotFoundException;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.ErrorInfo;
@@ -54,6 +55,20 @@ public class ErrorController extends ResponseEntityExceptionHandler {
     @ResponseBody
     public ErrorInfo processFilmNotFoundException(FilmNotFoundException e) {
         log.debug("Can not find film: {}", e.getMessage());
+        return new ErrorInfo(e.getMessage());
+    }
+
+    /**
+     * Обработчик RatingNotFoundException
+     *
+     * @param e Эксепшн
+     * @return Объект, содержащий сообщение об ошибке
+     */
+    @ExceptionHandler(RatingNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorInfo processRatingNotFoundException(RatingNotFoundException e) {
+        log.debug("Can not find rating: {}", e.getMessage());
         return new ErrorInfo(e.getMessage());
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/repository/impl/RatingRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/impl/RatingRepositoryImpl.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.filmorate.mapper.RatingMapper;
 import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.repository.RatingRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,6 +21,10 @@ public class RatingRepositoryImpl implements RatingRepository {
     private static final String SQL_GET_RATING_BY_FILM_ID = "SELECT id, name FROM public.rating "
             + "WHERE id = (SELECT rating_id FROM public.film where id = :id)";
 
+    private static final String SQL_GET_ALL_RATINGS = "SELECT id, name FROM public.rating";
+
+    private static final String SQL_GET_RATING_BY_ID = "SELECT id, name FROM public.rating WHERE id = :id";
+
 
     @Override
     public Optional<Rating> getByFilmId(Integer id) {
@@ -27,5 +32,18 @@ public class RatingRepositoryImpl implements RatingRepository {
 
         params.addValue("id", id);
         return jdbcTemplate.query(SQL_GET_RATING_BY_FILM_ID, params, ratingMapper).stream().findFirst();
+    }
+
+    @Override
+    public List<Rating> getAllRatings() {
+        return jdbcTemplate.query(SQL_GET_ALL_RATINGS, ratingMapper);
+    }
+
+    @Override
+    public Optional<Rating> getRatingById(Integer id) {
+        var params = new MapSqlParameterSource();
+
+        params.addValue("id", id);
+        return jdbcTemplate.query(SQL_GET_RATING_BY_ID, params, ratingMapper).stream().findFirst();
     }
 }
