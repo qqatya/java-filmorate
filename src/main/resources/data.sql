a9a1b187ece0fc8d47a0c9d7bce4207f0915a158
qqatya <catherineburger@yandex.ru>
2023-07-06 19:59:47 +0300

    fix data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO PUBLIC.GENRE (NAME) VALUES ('Комедия'),
-                                        ('Драма'),
-                                        ('Мультфильм'),
-                                        ('Триллер'),
-                                        ('Документальный'),
-                                        ('Боевик');
+MERGE INTO PUBLIC.GENRE KEY (ID) VALUES (1, 'Комедия'),
+                                        (2, 'Драма'),
+                                        (3, 'Мультфильм'),
+                                        (4, 'Триллер'),
+                                        (5, 'Документальный'),
+                                        (6, 'Боевик');
 
-INSERT INTO PUBLIC.RATING (NAME) VALUES ('G'),
-                                        ('PG'),
-                                        ('PG-13'),
-                                        ('R'),
-                                        ('NC-17');
+MERGE INTO PUBLIC.RATING KEY (ID) VALUES (1, 'G'),
+                                         (2, 'PG'),
+                                         (3, 'PG-13'),
+                                         (4, 'R'),
+                                         (5, 'NC-17');
