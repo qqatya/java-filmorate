d2dfd70299441a87dbc533859a3c39c46d00a6f5
qqatya <catherineburger@yandex.ru>
2023-08-02 20:27:40 +0300

    fix style

--- src/main/java/ru/yandex/practicum/filmorate/exception/ReviewAlreadyRatedException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ReviewAlreadyRatedException.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ReviewAlreadyRatedException extends RuntimeException {
     public ReviewAlreadyRatedException(String message) {
-        super("Такая оценка рейтинга с id = " + message + " уже существует" );
+        super("Такая оценка рейтинга с id = " + message + " уже существует");
     }
 }
