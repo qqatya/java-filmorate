07f8fa80316f2dce774cc3c1f5b454eadd4e84fc
qqatya <catherineburger@yandex.ru>
2023-08-04 00:22:47 +0300

    refactor: перенести заполнение вложенных полей в маппер (фильмы)

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.repository.DirectorRepository;
 import ru.yandex.practicum.filmorate.repository.GenreRepository;
 import ru.yandex.practicum.filmorate.repository.RatingRepository;
 
@@ -13,8 +14,12 @@ import java.sql.SQLException;
 @Component
 @RequiredArgsConstructor
 public class FilmMapper implements RowMapper<Film> {
+
     private final GenreRepository genreRepository;
+
     private final RatingRepository ratingRepository;
+
+    private final DirectorRepository directorRepository;
 
     @Override
     public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -28,6 +33,7 @@ public class FilmMapper implements RowMapper<Film> {
                 .duration(rs.getLong("duration"))
                 .mpa(ratingRepository.getByFilmId(filmId).orElse(null))
                 .genres((genreRepository.getByFilmId(rs.getInt("id"))))
+                .directors(directorRepository.getByFilmId(filmId))
                 .build();
     }
 }
