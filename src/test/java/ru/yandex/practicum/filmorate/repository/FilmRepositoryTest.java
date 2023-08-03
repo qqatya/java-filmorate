825d388326f8e90fa9fbf7a498ea592e1888386c
Сергей Лисовский <19lis91@gmail.com>
2023-08-03 22:11:37 +0300

    исправил тесты и удалил бинарные файлы2

--- src/test/java/ru/yandex/practicum/filmorate/repository/FilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/repository/FilmRepositoryTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.repository;
 
 import lombok.RequiredArgsConstructor;
-
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -10,13 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.repository.impl.FilmRepositoryImpl;
-import ru.yandex.practicum.filmorate.repository.impl.GenreRepositoryImpl;
-import ru.yandex.practicum.filmorate.repository.impl.RatingRepositoryImpl;
 import ru.yandex.practicum.filmorate.repository.impl.UserRepositoryImpl;
-import ru.yandex.practicum.filmorate.service.GenreService;
 import ru.yandex.practicum.filmorate.service.RatingService;
 
 import java.time.LocalDate;
@@ -121,13 +116,24 @@ class FilmRepositoryTest {
 
     }
 
-//    @Test
-//    @Order(6)
-//    void getPopularFilms() {
-//        List<Film> films = filmRepository.getPopularFilms(1);
-//
-//        assertEquals(1, films.size());
-//    }
+    @Test
+    @Order(6)
+    void getPopularFilms() {
+        String name = "Shutter Island";
+
+        Film film = filmRepository.insertFilm(Film.builder()
+                .id(1)
+                .name(name)
+                .description(DESCRIPTION)
+                .releaseDate(RELEASE_DATE)
+                .duration(DURATION)
+                .mpa(ratingService.getRatingById(1))
+                .build());
+
+        List<Film> films = filmRepository.getPopularFilms(1, null, 2010);
+
+        assertEquals(1, films.size());
+    }
 
     @Test
     @Order(7)
